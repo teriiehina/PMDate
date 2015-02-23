cde9d92d399dad67aae3d7cbe11f0829a6b1aaeb
Peter Meuel <pmeuel@pagesjaunes.fr>
2015-02-23 11:05:46 +0100

    Fix the `xctool` arguments in the coveralls.sh script

--- script/coveralls.sh
+++ script/coveralls.sh
@@ -10,11 +10,11 @@ trim()
 }
 
 # declare BUILT_PRODUCTS_DIR CURRENT_ARCH OBJECT_FILE_DIR_normal SRCROOT OBJROOT 
-declare -r xctoolVars=$(xctool -showBuildSettings | egrep '(BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' |  egrep -v 'Pods')
+declare -r xctoolVars=$(xctool -project PMDate.xcodeproj -scheme PMDate -configuration Debug -showBuildSettings | egrep '(BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' |  egrep -v 'Pods')
 while read line; do
 	declare key=$(echo "${line}" | cut -d "=" -f1)
 	declare value=$(echo "${line}" | cut -d "=" -f2)
-	printf -v "`trim ${key}`" "`trim ${value}`" # https://sites.google.com/a/tatsuo.jp/programming/Home/bash/hentai-bunpou-saisoku-masuta
+  printf -v "`trim ${key}`" "`trim ${value}`" # https://sites.google.com/a/tatsuo.jp/programming/Home/bash/hentai-bunpou-saisoku-masuta
 done < <( echo "${xctoolVars}" )
 
 declare -r gcov_dir="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
